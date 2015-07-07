76ab574b831888dfdd9d0ac57aa2b1f6384442bb
rangerbiru <ahmadhidayatullah95@gmail.com>
2015-07-08 01:52:12 +0800

    Create input.php

---
+++ input.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<head>
+<body>
+<form action="tangkap.php" method="post">
+judul:<input type="text" name="judul"><br><br>
+isi : <textarea cols="40" rows="7" name="isi"></textarea><input type="submit" value="save">
+</form>
+</body>
+</html>
